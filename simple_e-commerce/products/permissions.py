1d5b15d92d9f827b18c420d203db59ddda6d7b0e
RenanCosta2 <renanleitedacosta@gmail.com>
2024-11-22 16:35:29 -0300

    <style>: Change dir name from "simple_e-ecommerce" to "e-commerce"

--- simple_e-commerce/products/permissions.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import permissions
-
-class IsSuperUser(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        return request.user and request.user.is_superuser
--- simple_e-commerce/users/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class Users(AbstractUser):
-    cpf = models.CharField(max_length=14, unique=True)
-    email = models.EmailField(unique=True)
