39f8152917532498dacdd66ad2ea05bf7b13cd00
RenanCosta2 <renanleitedacosta@gmail.com>
2024-11-22 18:02:58 -0300

    <fix>: Fixed configs to use new dir name "e_commerce"

--- e-commerce/cart/models.py
+++ e-commerce/cart/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from users.models import Users
+from products.models import Products
 
 # Create your models here.
+class Carts(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    data_updated = models.DateTimeField(auto_now=True)
+
+class ItemCart(models.Model):
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
