43b55f3c7493ba33ea355e335e9e223e0144e7e8
RenanCosta2 <renanleitedacosta@gmail.com>
2024-11-26 09:38:05 -0300

    <feat>: create a cart for a user when is created

--- e-commerce/users/views.py
+++ e-commerce/users/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, status
 from .serializers import UsersSerializer
 from .models import Users
+from cart.models import Carts
 from rest_framework.response import Response
 
 class UsersViewSet(viewsets.ModelViewSet):
@@ -13,7 +14,8 @@ class UsersViewSet(viewsets.ModelViewSet):
         if serializer.is_valid():
             try:
                 user = serializer.save()
+                Carts.objects.create(user=user)
                 return Response({'message': 'User registered successfully!'}, status=status.HTTP_201_CREATED)
-            except:
+            except Exception as e:
                 return Response({'error': 'Error registering user: ' + str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
