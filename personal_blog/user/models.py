413a0b25cdecb94d8d2213aed0bea31cf2dd1bc9
RenanCosta2 <renanleitedacosta@gmail.com>
2024-12-13 16:11:01 -0300

    <refactor>: diretórios de outros projetos removidos e  e-commerce movido para o diretório raiz

--- personal_blog/user/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class CustomUser(AbstractUser):
-    bio = models.TextField(blank=True)
-    birth_date = models.DateField(null=True, blank=True)
-
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = [] 
--- to_do_list/task/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class TaskConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'task'
-
-    def ready(self):
-        import task.signals
