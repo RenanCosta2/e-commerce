757f964b86e3b7996cf1126c39c3661c07e9b1e9
RenanCosta2 <renanleitedacosta@gmail.com>
2024-12-18 22:45:49 -0300

    fix: ensuring that the min value is a decimal

--- products/models.py
+++ products/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 from django.core.validators import MinValueValidator
+from decimal import Decimal
 
 class Products(models.Model):
     name = models.CharField(max_length=255)
     category = models.CharField(max_length=100)
     description = models.TextField()
-    value = models.DecimalField(max_digits=15, decimal_places=2, validators=[MinValueValidator(0)])
+    value = models.DecimalField(
+    max_digits=15,
+    decimal_places=2,
+    validators=[MinValueValidator(Decimal('0.0'))] 
+    )
     storage = models.IntegerField(validators=[MinValueValidator(0)])
     data_created = models.DateTimeField(auto_now_add=True)
     data_updated = models.DateTimeField(auto_now=True)
